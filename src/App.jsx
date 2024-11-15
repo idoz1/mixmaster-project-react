249b96b4c18a848dee42b8d76a738991bcd84dda
idoz1 <169044973+idoz1@users.noreply.github.com>
2024-11-15 23:04:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  About,
+  HomeLayout,
+  Landing,
+  Error,
+  Cocktail,
+  Newsletter,
+} from './pages'
+
+import { loader as landingLoader } from './pages/Landing'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        loader: landingLoader,
+      },
+      {
+        path: 'cocktail',
+        element: <Cocktail />,
+      },
+      {
+        path: 'newsletter',
+        element: <Newsletter />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
+  },
+])
+
+const App = () => {
+  return <RouterProvider router={router} />
+}
+export default App
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import { useLoaderData } from 'react-router-dom'
+
+export const loader = async () => {
+  return 'something'
+}
+
+const Landing = () => {
+  const data = useLoaderData()
+  console.log(data)
+
+  return <div>Landing</div>
+}
+
+export default Landing
